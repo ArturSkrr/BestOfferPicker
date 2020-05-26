739d0fae4643368447104f134cff0ef4bc672752
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2020-05-26 13:59:51 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -24,31 +24,36 @@ def get_prices(offers):
             maxs.append(max)
     return second_mins, mins, maxs
 
-df = pd.read_csv("Raw Data.csv")
-df = df.replace({'0':np.nan, 0:np.nan, 0.00:np.nan})
-df['Supplier Best Offer'] = df.apply(lambda x: x.isin(df.min(axis=1)), axis=1).apply(lambda x: list(df.columns[x]), axis=1)
-suppliers = df.columns[1:-1].to_numpy()
-products = df['Product'].to_numpy()
-best_offers = df['Supplier Best Offer'].to_numpy()
-df.drop(['Supplier Best Offer'],axis=1,inplace=True)
-df.drop(['Product'],axis=1,inplace=True)
-offers  = df.to_numpy()
+def main():
+    df = pd.read_csv("Raw Data.csv")
+    df = df.replace({'0':np.nan, 0:np.nan, 0.00:np.nan})
 
-second_mins, mins, maxs = get_prices(offers)
-percentages_sec_mins = count_percentage(mins, second_mins)
-percentages_maxs = count_percentage(mins, maxs)
-savings = [(max - min) for max, min in zip(maxs, mins)]
+    df['Supplier Best Offer'] = df.apply(lambda x: x.isin(df.min(axis=1)), axis=1).apply(lambda x: list(df.columns[x]), axis=1)
+    suppliers = df.columns[1:-1].to_numpy()
+    products = df['Product'].to_numpy()
+    best_offers = df['Supplier Best Offer'].to_numpy()
+    df.drop(['Supplier Best Offer'],axis=1,inplace=True)
+    df.drop(['Product'],axis=1,inplace=True)
+    offers  = df.to_numpy()
 
-outcome_dict = {'Porduct': products,
-                'Supplier Best Offer': best_offers,
-                'Best Price': mins,
-                'Second Best Price': second_mins,
-                'Percentage Second': percentages_sec_mins,
-                'Max Price': maxs,
-                'Percentage Nax': percentages_maxs,
-                'Saving': savings
-}
+    second_mins, mins, maxs = get_prices(offers)
+    percentages_sec_mins = count_percentage(mins, second_mins)
+    percentages_maxs = count_percentage(mins, maxs)
+    savings = [(max - min) for max, min in zip(maxs, mins)]
 
-outcome_df = pd.DataFrame(outcome_dict)
+    outcome_dict = {'Porduct': products,
+                    'Supplier Best Offer': best_offers,
+                    'Best Price': mins,
+                    'Second Best Price': second_mins,
+                    'Percentage Second': percentages_sec_mins,
+                    'Max Price': maxs,
+                    'Percentage Nax': percentages_maxs,
+                    'Saving': savings
+    }
 
-print(outcome_df)
+    outcome_df = pd.DataFrame(outcome_dict)
+
+    print(outcome_df)
+
+if __name__ == "__main__":
+    main()
